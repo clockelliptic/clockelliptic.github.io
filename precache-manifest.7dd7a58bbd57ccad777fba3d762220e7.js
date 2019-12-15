2b5d4b312922c72be32601b46878a245c5f5ffa6
torch <harall09@evergreen.edu>
2019-12-15 18:31:53 -0500

    inital deploy

---
+++ precache-manifest.7dd7a58bbd57ccad777fba3d762220e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d637292af95dce5aee6734a447ef81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36899a0777384ff491d1",
+    "url": "/static/css/main.99df0da6.chunk.css"
+  },
+  {
+    "revision": "f8d3f3c2adfd67b2c7b5",
+    "url": "/static/js/2.b654f447.chunk.js"
+  },
+  {
+    "revision": "11ff426a2c7334f60fcd2eb48e081a6f",
+    "url": "/static/js/2.b654f447.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36899a0777384ff491d1",
+    "url": "/static/js/main.2002ce60.chunk.js"
+  },
+  {
+    "revision": "15238a5e4d2aaeef90e0",
+    "url": "/static/js/runtime-main.f368932b.js"
+  },
+  {
+    "revision": "dae5da99437050b9d9eb5f811a22770e",
+    "url": "/static/media/poro_snow.dae5da99.png"
+  },
+  {
+    "revision": "3b0be098f09047b140b8dba8eb45c4a1",
+    "url": "/static/media/summonersrift.3b0be098.svg"
+  }
+]);
